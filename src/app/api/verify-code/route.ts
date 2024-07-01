70917081d7d0dc000f20f59e34a302f15ecf21c9
Ayush <ayushkhanduri2612@gmail.com>
2024-07-01 17:17:12 +0530

    Add code verification checks

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,74 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { z } from "zod";
+import { verifySchema } from "@/schemas/verifySchema";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, code } = await request.json();
+    const decodedUsername = decodeURIComponent(username);
+
+    const result = verifySchema.safeParse({ code });
+    // console.log(result);
+    if (!result.success) {
+      const codeErrors = result.error.format().code?._errors || [];
+      return Response.json(
+        {
+          success: false,
+          message:
+            codeErrors?.length > 0
+              ? codeErrors.join(", ")
+              : "Invalid verification code",
+        },
+        { status: 400 }
+      );
+    }
+
+    const user = await UserModel.findOne({
+      decodedUsername,
+    });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 400 }
+      );
+    }
+
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+
+      return Response.json(
+        { success: true, message: "Account verified successfully" },
+        { status: 200 }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message:
+            "Verification code has expired, please signup again to get a new code",
+        },
+        { status: 400 }
+      );
+    } else {
+      return Response.json(
+        { success: false, message: "Incorrect Verification code" },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    console.error("Error verifying user", error);
+    return Response.json(
+      { success: false, message: "Error verifying user" },
+      { status: 500 }
+    );
+  }
+}
